98b021caf1b813314fc2ade3eba5752373c047c9
Bojan Piskulić <bpiskulic@qubitsoft.rs>
2023-08-14 02:06:34 +0200

    Cleanup account controller

--- CloudSalesSystem.Api/Controllers/Accounts/AccountsController.cs
+++ CloudSalesSystem.Api/Controllers/Accounts/AccountsController.cs
@@ -1,7 +1,4 @@
-﻿using CloudSalesSystem.Application.Abstractions.CloudComputing;
-using CloudSalesSystem.Application.Abstractions.CloudComputing.Models;
-
-namespace CloudSalesSystem.Api.Controllers.Accounts;
+﻿namespace CloudSalesSystem.Api.Controllers.Accounts;
 
 [Route("api/[controller]")]
 [ApiController]
@@ -20,10 +17,6 @@ public class AccountsController : ControllerBase
     public async Task<IActionResult> GetAccounts([FromQuery] GetAccountsQuery request, CancellationToken cancellationToken)
     {
         var result = await _sender.Send(request, cancellationToken);
-
-        var test = await _cloudComputingService.GetAvailableServices(cancellationToken);
-        var test2 = await _cloudComputingService.OrderComputingServiceItem(new OrderServiceItemRequest(test.First().Id, 4), cancellationToken);
-
         return Ok(result);
     }
 }
--- CloudSalesSystem.Api/GlobalUsings.cs
+++ CloudSalesSystem.Api/GlobalUsings.cs
@@ -2,6 +2,8 @@
 
 global using CloudSalesSystem.Api.Extensions;
 global using CloudSalesSystem.Application;
+global using CloudSalesSystem.Application.Abstractions.CloudComputing;
+global using CloudSalesSystem.Application.Abstractions.CloudComputing.Models;
 global using CloudSalesSystem.Application.Accounts.GetAccounts;
 global using CloudSalesSystem.Infrastructure;
 global using MediatR;
